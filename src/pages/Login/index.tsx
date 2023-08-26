ba6c22adf141ac8e291b4c2c592a30d9c6498ca6
Samuel Vitor de Carvalho <samuelchack6@gmail.com>
2023-08-25 22:31:16 -0300

    Finalized login and create custom hook usePersistedState

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,26 +9,60 @@ import {
   Group,
   LinkRegister,
 } from "./styles";
+import { FormEvent, useState } from "react";
+import { useAuthentication } from "../../contexts/Authentication";
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
+  const { signIn } = useAuthentication();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleRegister = () => {
     navigate("/register");
   };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const { result } = await signIn({ email, password });
+
+    if (result === "success") alert("Logado com sucesso!");
+    if (result === "error") alert("Falha ao fazer login!");
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <h1>Entrar</h1>
 
         <Group>
-          <Label htmlFor="">Endereço de e-mail</Label>
-          <Input type="text" placeholder="Digite seu e-mail" required />
+          <Label htmlFor="email">Endereço de e-mail</Label>
+          <Input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Digite seu e-mail"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </Group>
 
         <Group>
           <Label htmlFor="">Sua senha secreta</Label>
-          <Input type="password" placeholder="Digite sua senha" required />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Digite sua senha"
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
         </Group>
 
         <Button>Fazer login</Button>
